d69256d605e7135c1395bf743549fbfbe35c43db
Kristine Enerio <kristine.enerio@pendo.io>
2025-08-29 17:59:59 -0400

    Setting up navigation basics

---
+++ src/page/save-the-date.tsx
@@ -0,0 +1,24 @@
+import gingko from '@/media/gingko_transparent_background.png'
+
+function SaveTheDate() {
+  return (
+    <div className="save-the-date-content flex flex-col items-center justify-center">
+      <h1 className="font-bold">Will & Kristine</h1>
+      <img src={gingko} alt="logo" className="w-1/3 pt-10 pb-10"/>
+      <div>
+        
+      </div>
+      <h2 className="dates font-bold pb-5">
+        <p>June 17, 2026</p>
+      </h2>
+      <h3 className="location pb-5">
+        <p className="font-bold">Mount Mitchell Eco Retreat</p>
+        <p>430 Otter Rock Road</p>
+        <p>Burnsville, NC 28714</p>
+      </h3>
+      <div className="italic">Invitation and details to come</div>
+    </div>
+  )
+}
+
+export default SaveTheDate;
